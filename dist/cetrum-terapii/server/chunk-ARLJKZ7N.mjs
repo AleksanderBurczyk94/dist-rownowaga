ccd828b03c039de2f072b26b0a20058a7ccab29d
aburczyk <aleksander.burczyk@capgemini.com>
2024-10-27 18:17:30 +0100

    dist dodano

---
+++ dist/cetrum-terapii/server/chunk-ARLJKZ7N.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{f as m}from"./chunk-XG4GEVLC.mjs";import{Z as n,fb as o,gb as i,hb as p,sb as r}from"./chunk-IKYIHCDQ.mjs";var d=(()=>{class e{static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275cmp=n({type:e,selectors:[["app-blog"]],decls:3,vars:0,consts:[["title","Blog"]],template:function(t,a){t&1&&(p(0,"app-banner",0),o(1,"p"),r(2,"blog works!"),i())},dependencies:[m]})}}return e})();export{d as a};
